8c18b9aabfc0f8dc21fd2e31006e4c9dad296e2a
Daniellecr99 <Daniellecr99@gmail.com>
2021-12-06 22:43:41 -0700

    done

---
+++ converter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class converter {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double celsius  = sc.nextDouble();
+        double fahrenheight = ( celsius * 1.8 + 32);
+    
+      
+                        System.out.println(fahrenheight);  
+                        
+
+    }
+}
